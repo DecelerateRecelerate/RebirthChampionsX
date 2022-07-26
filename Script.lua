06b04637f354df3010c1d47bd949807386c234a3
DecelerateRecelerate <109950996+DecelerateRecelerate@users.noreply.github.com>
2022-07-26 18:29:01 +0200

    Create Script.lua

---
+++ Script.lua
@@ -0,0 +1,26 @@
+-- Create UI
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+
+local Window = Library.CreateLib("Rebirth Champions X", "Ocean")
+-- Tabs
+local automationsection = Window:NewTab("Auto") local automation = automationsection:NewSection("Automation")
+-- Main
+automation:NewToggle("AutoClicker", "Autoclicks for you", function(state)
+    if state then
+		_G.autoClicker = true;
+		        while _G.autoClicker == true do
+			game:GetService("ReplicatedStorage").Events.Click3:FireServer()
+			wait()
+        end
+    else
+		_G.autoClicker = false;
+    end
+end)
+
+
+
+
+--local args = {
+--    [1] = 1
+--}
+--game:GetService("ReplicatedStorage").Events.Rebirth:FireServer(unpack(args))
