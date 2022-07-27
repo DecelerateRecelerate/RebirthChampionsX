48636e7c8517ebf1511895716ecaada41b06ebc4
DecelerateRecelerate <109950996+DecelerateRecelerate@users.noreply.github.com>
2022-07-27 12:04:38 +0200

    Script.lua update - Codes
    Auto-use codes!

--- Script.lua
+++ Script.lua
@@ -5,6 +5,7 @@ local Window = Library.CreateLib("Rebirth Champions X", "Ocean")
 -- Tabs
 local automationsection = Window:NewTab("Automation") local automation = automationsection:NewSection("Automation")
 local autohatchsection = Window:NewTab("AutoHatch") local autohatch = autohatchsection:NewSection("Autohatch")
+local miscsection = Window:NewTab("Misc") local misc = miscsection:NewSection("Autohatch")
 -- Main
 automation:NewToggle("AutoClicker", "Autoclicks for you", function(state)
     if state then
@@ -31,4 +32,44 @@ game:GetService("ReplicatedStorage").Functions.Unbox:InvokeServer(unpack(args))
     else
 		_G.autohatchbasic3x = false;
     end
+end)
+misc:NewButton("ButtonText", "Redeems all codes in Rebirth Champions X.", function()
+		local args = {
+    [1] = "75m"
+    [2] = "fantasy"
+    [3] = "lab"
+    [4] = "kraken"
+    [5] = "150kthanks"
+    [6] = "60m"
+    [7] = "galactic"
+    [8] = "alien"
+    [9] = "mars"
+    [10] = "space"
+    [11] = "50m"
+    [12] = "hell"
+    [13] = "steampunk"
+    [14] = "easter2"
+    [15] = "100kthanks"
+    [16] = "easter"
+    [17] = "cave"
+    [18] = "75kthanks"
+    [19] = "spooky"
+    [20] = "void"
+    [21] = "nuclear"
+    [22] = "thanksfor50k"
+    [23] = "10mthanks"
+    [24] = "heaven"
+    [25] = "wow30000"
+    [26] = "20kthankyou"
+    [27] = "freeluckboost"
+    [28] = "wow10klikesthanks"
+    [29] = "freeclicksomg"
+    [30] = "5klikesthanks"
+    [31] = "wow2500likes"
+    [32] = "already1500likes"
+    [33] = "thanks500likes"
+    [34] = "RELEASE"
+}
+game:GetService("ReplicatedStorage").Events.Codes:FireServer(unpack(args))
+    print("Redeemed all codes!")
 end)
